1bed5974b19ee27d680dd11ed1ebfa5a9f3c88aa
John Agudelo <john.agudelo@INFMX-JAGUDELO.local>
2021-11-05 14:43:15 +0100

    feat(atoms): implement atom, selectors and atomfamily

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,18 +1,88 @@
+import {createContext, useState} from 'react'
+import {atom, useSetRecoilState, useRecoilValue} from 'recoil'
+import {Element, Rectangle} from './components/Rectangle/Rectangle'
 import {PageContainer} from './PageContainer'
-import {Rectangle} from './components/Rectangle/Rectangle'
 import {Toolbar} from './Toolbar'
 
+type ElementsContextType = {
+  elements: Element[]
+  addElement: () => void
+  setElement: SetElement
+}
+
+export const ElementsContext = createContext<ElementsContextType>({
+  elements: [],
+  addElement: () => {},
+  setElement: () => {},
+})
+
+/* type SelectedElementContextType = {
+  selectedElement: number | null
+  setSelectedElement: (index: number) => void
+} */
+
+/* export const SelectedElementContext = createContext<SelectedElementContextType>({
+  selectedElement: null,
+  setSelectedElement: () => {},
+}) */
+
+export const selectedElementState = atom<number | null>({
+  key: 'seletedElement',
+  default: null,
+})
+
+export type SetElement = (indexToSet: number, newElement: Element) => void
+
+export const elementState = atom<number[]>({
+  key: 'elements',
+  default: [],
+})
+
 function Canvas() {
-    return (
-        <PageContainer
-            onClick={() => {
-                console.log('Deselect all elements!')
-            }}
-        >
-            <Toolbar />
-            <Rectangle />
-        </PageContainer>
+  //   const [elements, setElements] = useState<Element[]>([])
+  const setSelectedElement = useSetRecoilState(selectedElementState)
+  const elements = useRecoilValue(elementState)
+  // const [selectedElement, setSelectedElement] = useState<number | null>(null)
+
+  /* const setElement: SetElement = (indexToSet, newElement) => {
+    setElements(
+      elements.map((element, index) => {
+        if (indexToSet === index) return newElement
+        return element
+      }),
     )
+  }
+
+  const addElement = () => {
+    setElements((elements) => {
+      return [
+        ...elements,
+        {
+          style: {
+            position: {top: 100 + elements.length * 30, left: 100 + elements.length * 30},
+            size: {width: 100, height: 100},
+          },
+        },
+      ]
+    })
+  } */
+
+  return (
+    // <SelectedElementContext.Provider value={{selectedElement, setSelectedElement}}>
+    // <ElementsContext.Provider value={{elements, addElement, setElement}}>
+    <PageContainer
+      onClick={() => {
+        setSelectedElement(null)
+      }}
+    >
+      <Toolbar />
+      {elements.map((id) => (
+        <Rectangle key={id} id={id} />
+      ))}
+    </PageContainer>
+    // </ElementsContext.Provider>
+    // </SelectedElementContext.Provider>
+  )
 }
 
 export default Canvas
--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,29 @@ import './index.css'
 import Canvas from './Canvas'
 import {ChakraProvider} from '@chakra-ui/react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {RecoilRoot} from 'recoil'
+
+import {Selectors} from './examples/Selectors'
 
 ReactDOM.render(
-    <React.StrictMode>
-        <ChakraProvider>
-            <Router>
-                <Switch>
-                    <Route>
-                        <Canvas />
-                    </Route>
-                </Switch>
-            </Router>
-        </ChakraProvider>
-    </React.StrictMode>,
-    document.getElementById('root'),
+  <React.StrictMode>
+    <RecoilRoot>
+      <ChakraProvider>
+        <Router>
+          <Switch>
+            {/* <Route path="/example/atoms">
+              <Atoms />
+            </Route> */}
+            <Route path="/example/selectors">
+              <Selectors />
+            </Route>
+            <Route>
+              <Canvas />
+            </Route>
+          </Switch>
+        </Router>
+      </ChakraProvider>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById('root'),
 )
